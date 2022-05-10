4aaf47b38308ae73f418e53bfc2ffa662b2aa5c6
Billmetal <billmetal9@gmail.com>
2022-05-09 21:46:22 -0300

    Adicionada a requisição de busca de correntista pelo id .

--- src/main/java/com/dio/santander/bankline/api/controllers/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controllers/CorrentistaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,11 @@ public class CorrentistaController {
 	@PostMapping
 	public void save(@RequestBody NovoCorrentista correntista) {
 		service.save(correntista);
+	}
+	
+	@GetMapping("/{idCorrentista}")
+	public Correntista findById(@PathVariable("idCorrentista") Integer idCorrentista){
+		return repository.findById(idCorrentista).orElse(null);
 	}
 
 }
